dc3ee147e0272d87a8cddfae2069df553b97ca2f
Florin-Ciprian Bodin <orynider@rdslink.ro>
2020-02-24 00:47:46 +0200

    Update mx_textblock_html.php
    Prep for RC6

--- modules/mx_textblocks/mx_textblock_html.php
+++ modules/mx_textblocks/mx_textblock_html.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package MX-Publisher Module - mx_textblocks
-* @version $Id: mx_textblock_html.php,v 1.20 2013/06/28 15:36:45 orynider Exp $
+* @version $Id: mx_textblock_html.php,v 3.20 2020/02/24 01:47:45 orynider Exp $
 * @copyright (c) 2002-2008 [Jon Ohlsson] MX-Publisher Project Team
 * @license http://opensource.org/licenses/gpl-license.php GNU General Public License v2
 * @link http://mxpcms.sourceforge.net/
@@ -42,10 +42,36 @@ $message = $mx_block->get_parameters( 'Html' );
 include_once($mx_root_path . 'includes/mx_functions_tools.'.$phpEx);
 $mx_text = new mx_text();
 $mx_text->init(true, false, false);
+if (is_object($mx_page))
+{
+	// -------------------------------------------------------------------------
+	// Extend User Style with module lang and images
+	// Usage:  $mx_user->extend(LANG, IMAGES)
+	// Switches:
+	// - LANG: MX_LANG_MAIN (default), MX_LANG_ADMIN, MX_LANG_ALL, MX_LANG_NONE
+	// - IMAGES: MX_IMAGES (default), MX_IMAGES_NONE
+	// -------------------------------------------------------------------------
+	$mx_user->extend(MX_LANG_MAIN, MX_IMAGES_NONE);
+	$mx_page->add_copyright( 'MX-Publisher Text-Blocks Module' );
+}
+$message = !empty($lang[str_replace(' ', '_', $message)]) ? $lang[str_replace(' ', '_', $message)] : $message;
 
+if (strrpos($message, '_'))
+{
+	$lang_strings = explode(' ', $message);
+	$num_strings = count($lang_strings);
+	
+	$message_row = '';
+	for ($i = 0; $i < $num_strings; $i++)
+	{
+		$message_row .= ($lang[$lang_strings[$i]]) ? $lang[$lang_strings[$i]] : $lang_strings[$i];
+	}
+	$message = $message_row;
+}
 //
 // Decode for display
 //
+$title = !empty($lang[$title]) ? $lang[$title] : $title;
 $title = $mx_text->display($title);
 $message = $mx_text->display($message);
 
@@ -53,7 +79,7 @@ $message = $mx_text->display($message);
 // Start output of page
 //
 $template->set_filenames(array(
-	'body_block' => 'mx_textblock_html.tpl')
+	'body_block' => 'mx_textblock_html.'.$tplEx)
 );
 
 $template->assign_vars(array(
