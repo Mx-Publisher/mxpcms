a53a649449f323b9e23aadc04d4f1970c3283be1
Florin-Ciprian Bodin <orynider@rdslink.ro>
2020-02-25 04:11:47 +0200

    Update session.php

--- includes/sessions/rhea/session.php
+++ includes/sessions/rhea/session.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package Style
-* @version $Id: session.php,v 1.1 2014/07/07 20:38:17 orynider Exp $
+* @version $Id: session.php,v 3.1 2020/02/25 04:13:02 orynider Exp $
 * @copyright (c) 2002-2008 MX-Publisher Project Team & (C) 2005 The phpBB Group
 * @license http://opensource.org/licenses/gpl-license.php GNU General Public License v2
 * @link http://mxpcms.sourceforge.net/
